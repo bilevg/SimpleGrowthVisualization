1e975ff7009cef5d43d72a00b1dc236fdf5577bf
Gavril Bilev <allattention@gmail.com>
2018-04-19 23:01:14 -0400

    Cleanup, customized f-n to obtain shapefiles

--- Growthviz.R
+++ Growthviz.R
@@ -1,12 +1,13 @@
 library(cshapes)
-library(tidyr)
-library(dplyr)
-library(rworldmap)
-library(spdep)
-library(tripack)
-library(maptools)
+library(tidyverse)
+## library(tidyr)
+## library(dplyr)
+## library(rworldmap)
+## library(spdep)
+## library(tripack)
+## library(maptools)
 library(wbstats)
-library(ggplot2)
+## library(ggplot2)
 library(scales)
 ## library(gganimate)
 library(gpclib)
@@ -16,13 +17,16 @@ library(magick)
 gpclibPermit()
 options(digits=8, max.print=1000, scipen=13)
 
-## world bank indicator for gdp percent change annual
+## World Bank indicator for gdp percent change annual
 start = 1961
 end = 2016
+## truncate growth rate at:
+maximum = 6
+
 
 ####################################################################
 ## Data Prep
-####################################################################
+##p##################################################################
 ## ## get the GDPpc contant 2010$ and Population total indicators from Development Indicators
 wb.df = wb(indicator=c('NY.GDP.MKTP.KD.ZG', 'SP.POP.TOTL'), country='countries_only', start=start, end=end)
 
@@ -35,8 +39,8 @@ wb.df = wb.df %>%
     mutate(date = as.numeric(date)) %>%
     rename(Population = 'Population, total', ISO1AL3 = iso3c,
            GDPgrowth = 'GDP growth (annual %)') %>%
-    mutate(GDPgrowth = truncate_top(GDPgrowth, 6)) %>%
-    mutate(GDPgrowth = truncate_bottom(GDPgrowth, -6)) %>%
+    mutate(GDPgrowth = truncate_top(GDPgrowth, maximum)) %>%
+    mutate(GDPgrowth = truncate_bottom(GDPgrowth, -maximum)) %>%
     filter(Population >= 500000) %>%
     select(-Population)
 
@@ -49,19 +53,53 @@ wb.world = wb.world %>%
     mutate(date = as.numeric(date)) %>%
     rename(GDPgrowth = 'GDP growth (annual %)')
 
+#n###################################################################
+## Shape files (from package cshp) prep
+####################################################################
+## customize the built-in cshp function to include several African states later start dates
+my_cshp <- function (date = NA)
+{
+    path <- paste(system.file(package = 'cshapes'), 'shp/cshapes.shp',
+        sep = '/')
+    cshp.full <- readShapePoly(path, proj4string = CRS('+proj=longlat +ellps=WGS84'),
+                               IDvar = 'FEATUREID')
+    ## fix for several missing states (late start)
+    missing <- c('Angola', 'Zambia', 'Mozambique', 'Botswana',
+                 'Tanzania', 'Kenya', 'Zimbabwe', 'Uganda',
+                 'Algeria', 'Rwanda', 'Burundi', 'Malawi',
+                 'Swaziland', 'Lesotho', 'Sierra Leone',
+                 'Equatorial Guinea')
+    for(state in missing){
+    cshp.full$GWSYEAR[cshp.full$CNTRY_NAME == state] <- 1960
+    }
+    ## manual fix for Western Sahara (part of Morocco)
+    cshp.full$GWSYEAR[cshp.full$FEATUREID == 243] <- 1960
+    cshp.full$GWSYEAR[cshp.full$FEATUREID == 219] <- 2017
+    cshp.full$GWSYEAR[cshp.full$FEATUREID == 220] <- 2017
+    ##
+    cshp.full <- cshp.full[cshp.full$GWCODE >= 0, ]
+    startdate <- as.Date(paste(cshp.full$GWSYEAR, cshp.full$GWSMONTH,
+                               cshp.full$GWSDAY, sep = '-'))
+    enddate <- as.Date(paste(cshp.full$GWEYEAR, cshp.full$GWEMONTH,
+                             cshp.full$GWEDAY, sep = '-'))
+    cshp.part <- cshp.full[startdate <= date & enddate >=
+                           date, ]
+    ## slightly thin (simplify) the polygons for faster plotting
+    cshp.part <- thinnedSpatialPoly(cshp.part, tolerance=.1, minarea=100)
+    cshp.part
+}
+
 
 ####################################################################
 ## Plotting prep
 ####################################################################
 ## color calculation for timeline plot with colored background
-ncolors = 6
+ncolors = maximum
 levels = 5
 increment = (1/levels)
-x <- seq(0,1, length.out=ncolors * levels)
+x <- seq(0,1, length.out= ncolors*levels + 1)
 colors <- c(seq_gradient_pal('red4', 'yellow')(x), seq_gradient_pal('yellow', 'green4')(x))
 colors <- unique(colors)
-## add one more on top for the >6 values
-colors <- c(colors, rep(colors[length(colors)], 2))
 
 
 ## options for map plot
@@ -77,70 +115,59 @@ themeops <- theme(plot.background = element_rect(fill = 'white'),
                   legend.direction='vertical',
                   panel.grid=element_blank(),
                   legend.position='right',
-                  ## legend.key = element_rect(colour = 'black',  size = .2, linetype='solid')
-                  legend.key.height=unit(2,"line"),
+                  legend.key.height=unit(2,'line'),
                   legend.margin=margin(t=0, r=0, b=0, l=-.4, unit='in'),
                   plot.title=element_text(hjust=.5, size = 15))
 
 
 ####################################################################
-## Plotting prep
+## Plotting by year
 ####################################################################
 img <- image_graph(800, 600, res = 96)
-quartz(width=7, height=5)
+dev.new(width=7, height=5)
 ## mclapply(start:end, mc.cores = detectCores(),
 ##          mc.preschedule = TRUE, function(year){
 for (year in start:end){
     day = as.Date(paste(year, '6-30', sep='-'))
-    map.year = cshp(day, useGW=TRUE)
-    map.year <- thinnedSpatialPoly(map.year, tolerance=.3, minarea=100)
-    ## ## how many of the World Bank countries can we match? about 165 (pretty good)
-    ## sum(unique(wb.df$iso3c) %in% map.all@data$ISO1AL3)
+    map.year = cshp(day)
     ## drop unnecessary variables from cshape data
     map.year@data <- map.year@data %>% select(CNTRY_NAME, ISO1AL3)
     map.year@data$ISO1AL3 <- as.character(map.year@data$ISO1AL3)
-    ## ## mask for countries not in World Bank data
-    ## mask_missing <- map.year@data$ISO1AL3 %in% wb.df$ISO1AL3
     wb.year <- wb.df[wb.df$date == year, ]
-    ## wb.year$GDPgrowth[wb.year$GDPgrowth > 6] <- 6
-    ## wb.year$GDPgrowth[wb.year$GDPgrowth < -6] <- -6
-    ## wb.year$GDPgrowth <- cut_interval(wb.year$GDPgrowth, 10)
     ## merge the two
-    ## map.year@data <- data.frame(map.year@data, wb.year[match(map.year@data$ISO1AL3, wb.year$ISO1AL3), ])
     map.year@data <- join(map.year@data, wb.year, by='ISO1AL3', type='left')
-    map.year@data$id <- map.year@data$CNTRY_NAME
+    map.year$id <- map.year$CNTRY_NAME
+    ## prepare polygons for plotting
     mapofyear <- fortify(model=map.year, region = 'CNTRY_NAME')
     map.df <- join(mapofyear, map.year@data, by='id')
-    ##
+    ## prepare breaks and labels for map plot
+    breaks <- seq(-maximum, maximum,2); labels <- breaks
+    labels[1] <- paste('\u2264',labels[1], sep='')
+    labels[length(labels)] <- paste('\u2265',labels[length(labels)], sep='')
+    ## map plot
     map <- ggplot(map.df) + aes(long, lat, group=group, fill=GDPgrowth) +
         geom_polygon() + geom_path(color='black', lwd=.1) +
         coord_fixed(1.2) +
         xlim(-170, 185) + ylim(-54, 78) +
         scale_fill_gradient2(mid = 'yellow', low='red4',
                              high = 'green4',
-                             breaks=seq(-6,6,2), limits=c(-6, 6),
-                             labels=
-                                 paste(c(paste('\u2264',-6), seq(-4,4,2), paste('\u2265', 6)), '%', sep='')) +
-        guides(fill=guide_colorbar(title.position="top",
+                             breaks= breaks, limits=c(-maximum, maximum),
+                             labels= labels) +
+        guides(fill=guide_colorbar(title.position='top',
                                    title = NULL,
-                                   direction = "vertical")) +
+                                   direction = 'vertical')) +
         themeops + ggtitle('Annual Growth in GDPpc, by Country and Total')
-    ## to get the correct dimensions, taking into account coord_fixed:
-    ## (ylim[1] - ylim[2]) / (xlim[1] - xlim[2]) / coord_fixed = 2.224
-    ## ggsave(filename = paste('Growth_', year, '.png', sep=''), device = 'png', width = 4 * 2.224,
-    ##        height = 4,
-    ##        dpi = 150)
-    ## print(plot)
     ## timeline plot
     timeline <- ggplot(wb.world[1:match(year, wb.world$date), ],
                        aes(x=date, y=GDPgrowth, group=country)) +
-        annotate('rect', ymin = seq(-ncolors + increment/2, ncolors + increment/2, increment),
-                 ymax = seq(-ncolors + 1, ncolors + 1, increment),
-                 xmin = -Inf, xmax = Inf,
-                 fill=colors) +
+        annotate('rect', ymin = seq(-ncolors - increment/2, ncolors - increment/2, increment),
+                 ymax = seq(-ncolors + increment/2, ncolors + increment/2, increment),
+                 xmin = -Inf, xmax = Inf, fill=colors) +
+        annotate('rect', ymin = ncolors, ymax = Inf,
+                 xmin = -Inf, xmax = Inf, fill=colors[length(colors)]) +
         geom_line() + geom_point() +
         scale_x_continuous(breaks = seq(1960, 2016, 5)) +
-        scale_y_continuous(limits=c(-3, 8), breaks=seq(-2, 6, 2),
+        scale_y_continuous(limits=c(-3, 8), breaks=seq(-2, maximum, 2),
                            expand=c(0,0)) +
         expand_limits(x=2016) +
         theme_minimal() + theme(panel.grid.major = element_blank(),
@@ -148,43 +175,15 @@ for (year in start:end){
                                 axis.title.x=element_blank(),
                                 axis.title.y=element_blank())
     ## combine the two plots into one and print
-    ## png(paste('Growth_', year, '.png', sep=''), width = 6, height = 4, units='in', res = 96)
-    grid.arrange(map, timeline, nrow=2, heights=c(2,1.2))
-    ## map.v <- viewport(width = 1, height = .8, x = 0.5, y = 0.5)
-    ## timeline.v <- viewport(width = 1, height = .2, x = 0.5, y = 0.15)
-    ## save the file
-    ## png(paste('Growth_', year, '.png', sep=''), width = 4 * 2.224, height = 4)
-    ## print(map, vp =  viewport(layout.pos.row = 1, layout.pos.col = 1))
-    ## print(timeline, vp =  viewport(layout.pos.row = 2, layout.pos.col = 1))
-    ## dev.off()
+    grid.arrange(map, timeline, nrow=2, heights=c(2,1))
 }
-## )
-
-## try out magick package
 
 
 ## animation <- image_animate(image_morph(img,  frames=56*3), loop=1, fps=10)
-## animation <- image_animate(img, loop=1, fps = 1)
-## image_write(animation, "growth.gif")
-## gganimate(plot)
-
-## gganimate(plot, "output.gif")
+animation <- image_animate(img, loop=1, fps = 1)
+image_write(animation, 'growth.gif')
+dev.off()
 
 
 
-## function to produce world map based on year, matching WB using ISO1AL3
-map_codes <- allmap$ISO1AL3[!is.na(allmap$ISO1AL3)]
-duplicates <- map_codes[duplicated(map_codes)]
-allmap$ISO1AL3[allmap$ISO1AL3 %in% duplicates]
 
-for(year in start:end){
-    wb.year <- wb.df[wb.df$date == year, ]
-    for(iso in map_codes) {
-        if(iso %in% duplicates) {
-            ## pick one based on dates
-            year
-        }
-        else
-            ## match the only one
-            }
-}
